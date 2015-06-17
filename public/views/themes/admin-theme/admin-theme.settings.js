22ba384a9c2e284c83a26cac49ba35c5ff279d65
Adam <mr.adam.crockett@gmail.com>
2015-06-17 13:45:58 +0100

    Just working on the blocks and theme assets

--- public/views/themes/admin-theme/admin-theme.settings.js
+++ public/views/themes/admin-theme/admin-theme.settings.js
@@ -3,6 +3,10 @@ region["sidebar_first"] = "Admin primary sidebar"
 region["content"] = "Admin content"
 region["footer"] = "Admin footer"
 
-
 data["nav"] = {someData:"value", evenMore:"here is some data"}
 classes["nav"] = "navbar nav"
+
+
+
+//temporary test
+blocks["content"] = "Hello world"
