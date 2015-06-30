f25c02c6790740b962cd5a2e70b10170dbf4bcba
Adam <mr.adam.crockett@gmail.com>
2015-06-30 12:12:03 +0100

    added bower integration for libraries

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 var express = require('express'),
 		app = express(),
 		path = require('path'),
-		route = require('./routes/route.js'),
-		//magic
 		saphire = require('saphire').functions;
 
 var conf = {
 	port:3000,
-	root:path.normalize(__dirname +"/public/"),
-	templates:"jade"
+	root:path.normalize(__dirname +"/public/")
 }
 
 app.listen(conf.port);
 app.set('views', __dirname + '/public/views/');
-app.set('view engine', conf.templates);
 
-
-//KEEP
 saphire.start(app);
-
-//NEED TO CHANGE
-//saphireAdmin.routes(app, app.settings.views);
-
-
